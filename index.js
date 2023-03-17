95c4b476d04ec439020baee0a637575716a39d3b
Cynthia Nakhumicha Barasa <cynthiabarasa312@gmail.com>
2023-03-17 12:54:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+let eventType = "surprise";
+
+
+function writeCards(names, eventType) {
+    let namesArray = []
+    for (let i = 0; i < names.length; i++) {
+        namesArray.push(`Thank you, ${names[i]}, for the wonderful ${eventType} gift!`);
+    }
+    return namesArray;
+}
+
+writeCards(names, eventType);
+
+function countDown(num) {
+    let i = num;
+    while (i > 0) {
+        console.log(i--);
+    }
+    console.log(i);
+}
+
+countDown(10);
